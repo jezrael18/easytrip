a945b715274b2b31606327310c72dff7aa747fce
MrKabado <jersonjaybonghanoy@gmail.com>
2024-09-03 21:37:34 +0800

    first footer push

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Footer = () =>{
     return(
-        <div>
-                This is Footer
+        <div className="">
+                <div className="font-medium border-2 w-min">
+                    <h3>Contact Us</h3>
+                    <hr></hr><br></br>
+                    <p>Email Us</p>
+                    <p>support@barkota.com</p>
+                    <p>Call Us</p>
+                    <p>63+9915337918</p>
+                </div>
+                <div>
+                    <h3>Manuals</h3>
+                    <pre></pre>
+                    <p>Boarding Guidelines</p>
+                    <p>Payment Options</p>
+                    <p>Passenger Ticket Booking Manual</p>
+                    <p>Rolling Cargo Booking Manual</p>
+                </div>
         </div>
     )
 }
