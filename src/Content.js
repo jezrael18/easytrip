329be5aa15e2d00ac60fa56b7b6155a22f679093
jezrael18 <jezraelsuliano507@gmail.com>
2024-09-03 21:06:24 +0800

    first commit

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import Heading from "./Heading"
+import Footer from "./Footer";
+
+function Content (){
+  return(
+
+    <div className="text-black">
+      <Heading />
+      <div>
+        
+      </div>
+    <Footer/>
+    </div>
+
+  )
+}
+export default Content;
---
+++ src/Heading.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function heading () {
+    return(
+        <div className=" ">
+            <div className="text-black font-semibold ">
+            <ul className="flex flex-col">
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
+export default heading
