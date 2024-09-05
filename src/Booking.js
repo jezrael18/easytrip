7ab7ff26bdd7d02f0cd02e0ac2de26474c710df4
MrKabado <jersonjaybonghanoy@gmail.com>
2024-09-05 12:19:12 +0800

    added booking part

---
+++ src/Booking.js
@@ -0,0 +1,65 @@
+import Heading from "./Heading"
+import Footer from "./Footer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faCalendarDays, faUsers, faUserShield } from '@fortawesome/free-solid-svg-icons'
+
+function booking () {
+    return (
+        <div className="">
+           
+            <Heading />
+            <div id="main-holder" className="two-holder">
+                <div className="IamPicture">
+                    <img src="" alt="" />
+                </div>
+                <div id="bookingHolder" className="">
+                    <p className="text-center text-[35px] font-medium pb-[35px]">Online Booking</p>
+                    <table className="">
+                        <tr className="">
+                            <td className="text-[20px] pr-[20px] pl-[20px]"><p><FontAwesomeIcon className="text-cyan-800 text-lg" icon={faLocationDot} /> Origin</p></td>
+                            <td className="text-[20px] pr-[20px] pl-[20px]"><p><FontAwesomeIcon className="text-cyan-800 text-lg" icon={faLocationDot} /> Destination</p></td>
+                        </tr>
+                        <tr>
+                            <td className="text-[20px] pr-[20px] pl-[20px]">
+                                <select name="" id="">
+                                    <option value="">---</option>
+                                    <option value="CEB">CEB- CEBU</option>
+                                </select>
+                            </td>
+                            <td className="text-[20px] pr-[20px] pl-[20px]">
+                                <select name="" id="">
+                                    <option value="">---</option>
+                                    <option value="">---</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="text-[20px] pr-[20px] pl-[20px]"><p><FontAwesomeIcon className="text-cyan-800 text-lg" icon={faCalendarDays} /> Departure Date</p></td>
+                            <td className="text-[20px] pr-[20px] pl-[20px]"><p><FontAwesomeIcon className="text-cyan-800 text-lg"  icon={faUsers} /> Adult/Child</p></td>
+                        </tr>
+                        <tr>
+                            <td className="text-[20px] pr-[20px] pl-[20px]">
+                                <select name="" id="">
+                                    <option value="">---</option>
+                                </select>
+                            </td>
+                            <td className="text-[20px] pr-[20px] pl-[20px]">
+                                <select name="" id="">
+                                    <option value="">---</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <br />
+                        <p className="ml-[20px]"><FontAwesomeIcon className="text-cyan-800 text-lg" icon={faUserShield} /> With Insurrances</p>
+                        <input id="insurance-radio-yes" name="insurance" type="radio" className="ml-[20px] hover:cursor-pointer"/> &nbsp; <label htmlFor="insurance-radio-yes">Yes</label> &nbsp;&nbsp;
+                        <input id="insurance-radio-no" name="insurance" type="radio" className="hover:cursor-pointer" /> &nbsp; <label htmlFor="insurance-radio-no">Gusto Na Mamatay</label>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default booking
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Content from './Content';
+import Booking from './Booking';
+
 import reportWebVitals from './reportWebVitals';
+import Content from './Content';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Content />
+    <Booking />
   </React.StrictMode>
 );
 
